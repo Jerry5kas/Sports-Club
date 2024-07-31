18876650c52bc7572b5ec9fa69a0e5725f139d17
azhagiri <azhagirishankar5@gmail.com>
2024-07-31 16:13:07 +0530

    #2 project Files

---
+++ app/Livewire/SportsClub/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\SportsClub;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.sports-club.index');
+    }
+}
